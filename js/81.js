bf8e873d2a7873637f30facf4a930a48a1be815f
MishaIgn <ignatevmike@gmail.com>
2022-06-10 12:40:25 -0500

    80-81

---
+++ js/81.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: 'frame',
+    legs: 4,
+    tails: 1,
+    friends: ["Water", "Shwater"]
+  
+    // Only change code above this line
+  };
